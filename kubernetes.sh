9f4a8a506e6fdd1d311cec95ce97d6d3b1835564
Laurence Elsdon <git@elsdon.me>
2023-07-23 14:45:45 +0100

    feat(kubernetes): Colorize the output and add error handling

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,31 +1,96 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 # Install kubeadm, kubelet, and kubectl
 # Source: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
 # With amendment: https://github.com/kubernetes/website/issues/42144
 
-echo "➡️  Install apt dependencies"
-sudo apt-get update
-sudo apt-get install --quiet --yes \
-  ca-certificates \
-  curl \
-  gnupg
+echo-success () {
+  echo -e "\e[32m ✔  $1\e[0m" >&2
+}
 
-echo "➡️  Download the public signing key"
-sudo curl --fail --silent --show-error --location \
-  --output /etc/apt/keyrings/kubernetes-archive-keyring.gpg \
-  https://dl.k8s.io/apt/doc/apt-key.gpg 
+echo-information () {
+  echo -e "\e[34m ⓘ  $1\e[0m" >&2
+}
 
-echo "➡️  Add the Kubernetes apt repository"
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo-error () {
+  echo -e "\e[31m !  $1\e[0m" >&2
+}
 
-echo "➡️  Install kubelet, kubeadm and kubectl, and pin their version"
-sudo apt-get update
-sudo apt-get install --yes \
-  kubelet \
-  kubeadm \
-  kubectl
+echo-warning () {
+  echo -e "\e[33m ⚠  $1\e[0m" >&2
+}
+
+echo-subsidiary () {
+  echo -e "\e[37m    $1\e[0m" >&2
+}
+
+echo-step () {
+  echo -e "\e[0m »  $1\e[0m" >&2
+}
+
+validate-is-root() {
+  if [[ $(/usr/bin/id -u) -ne 0 ]]
+  then
+    echo-error "Root is required"
+    exit 1
+  fi
+}
+
+validate-is-root
+
+echo-information "Installing kubeadm, kubelet, and kubectl"
+
+echo-step "Installing apt dependencies"
+if ! (
+  set -e
+  apt-get update > /dev/null
+  apt-get install --quiet --yes \
+    ca-certificates \
+    curl \
+    gnupg
+)
+then
+  echo-error "Failed to install apt dependencies"
+  exit 1
+fi
+
+echo-step "Setup the Kubernetes repository"
+if ! (
+  set -e
+  curl --fail --silent --show-error --location \
+    --output /etc/apt/keyrings/kubernetes-archive-keyring.gpg \
+    https://dl.k8s.io/apt/doc/apt-key.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+)
+then
+  echo-error "Failed to setup the Kubernetes repository"
+  exit 1
+fi
+
+echo-step "Install kubelet, kubeadm and kubectl"
+if ! (
+  set -e
+  apt-get update > /dev/null
+  apt-get install --quiet --yes \
+    kubelet \
+    kubeadm \
+    kubectl
+)
+then
+  echo-error "Failed to install kubelet, kubeadm and kubectl"
+  exit 1
+fi
 
 # TODO: We probably need to fetch a specific version..
-sudo apt-mark hold kubelet kubeadm kubectl
+echo-step "Pin versions"
+if ! apt-mark hold kubelet kubeadm kubectl
+then
+  echo-error "Failed to pin versions"
+  exit 1
+fi
+
+echo-success "Installed ${COMMAND}"
+echo-subsidiary "$(kubelet --version)"
+echo-subsidiary "$(kubeadm --version)"
+echo-subsidiary "$(kubectl --version)"
