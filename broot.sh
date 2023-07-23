3fb4c19e0fc943bc3f70135d74661841111a096a
Laurence Elsdon <git@elsdon.me>
2023-07-23 13:34:56 +0100

    feat(broot): Simplify and colorize the output

--- broot.sh
+++ broot.sh
@@ -1,35 +1,75 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 COMMAND="broot"
 SOURCE="https://dystroy.org/broot/download/x86_64-linux/broot"
 DESTINATION="/usr/local/bin/${COMMAND}"
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]
+echo-success () {
+  echo -e "\e[32m ✔  $1\e[0m" >&2
+}
+
+echo-information () {
+  echo -e "\e[34m ⓘ  $1\e[0m" >&2
+}
+
+echo-error () {
+  echo -e "\e[31m !  $1\e[0m" >&2
+}
+
+echo-warning () {
+  echo -e "\e[33m ⚠  $1\e[0m" >&2
+}
+
+echo-subsidiary () {
+  echo -e "\e[37m    $1\e[0m" >&2
+}
+
+echo-step () {
+  echo -e "\e[0m »  $1\e[0m" >&2
+}
+
+validate-is-root() {
+  if [[ $(/usr/bin/id -u) -ne 0 ]]
+  then
+    echo-error "Root is required"
+    exit 1
+  fi
+}
+
+validate-not-installed() {
+  WHICH=$(which "${COMMAND}")  
+  if [[ "${WHICH}" != "" ]]
+  then
+    echo-warning "${COMMAND} is already installed:"
+    echo-subsidiary "Path: ${WHICH}"
+    exit 2
+  fi
+}
+
+validate-is-root
+validate-not-installed
+
+echo-information "Installing ${COMMAND}"
+echo-subsidiary "Source: ${SOURCE}"
+echo-subsidiary "Destination: ${DESTINATION}"
+
+echo-step "Downloading and installing"
+if ! curl "${SOURCE}" \
+      --output "${DESTINATION}" \
+       --show-error \
+       --silent
 then
-  echo "❗  Failed to install. Root is required." >&2
+  echo-error "Failed to download package"
   exit 1
 fi
 
-set +e
-WHICH=$(which "${COMMAND}")
-set -e
-
-if [[ "${WHICH}" != "" ]]
+if ! chmod +x "${DESTINATION}"
 then
-  echo "❗  Failed to install. ${COMMAND} is already installed:" >&2
-  echo "${WHICH}"
+  echo-error "Failed to grant execution permission"
   exit 1
 fi
 
-echo "Installing ${COMMAND}"
-echo "  Source: ${SOURCE}"
-echo "  Destination: ${DESTINATION}"
-
-sudo curl "${SOURCE}" \
-  --output "${DESTINATION}" \
-  --progress-bar
-sudo chmod +x "${DESTINATION}"
-
-echo "Complete. Confirm with:"
-echo "  ${COMMAND} --version"
+VERSION=$(${COMMAND} --version)
+echo-success "Installed ${COMMAND}"
+echo-subsidiary "${VERSION}"
