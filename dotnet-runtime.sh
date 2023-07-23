4dce1a19d52441e3091b104d529afe073d05f553
Laurence Elsdon <git@elsdon.me>
2023-07-23 14:32:06 +0100

    feat(dotnet-runtime): Colorize the output and add error handling

--- dotnet-runtime.sh
+++ dotnet-runtime.sh
@@ -1,23 +1,81 @@
 #!/bin/bash
-set -euo pipefail
 
 # Install .NET Runtime
 # Source: https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
 
-echo "➡️  Add the Microsoft package repository"
-declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+COMMAND="dotnet"
+PACKAGE="dotnet-runtime-7.0"
 
-# Download Microsoft signing key and repository
-wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+echo-success () {
+  echo -e "\e[32m ✔  $1\e[0m" >&2
+}
 
-# Install Microsoft signing key and repository
-sudo dpkg -i packages-microsoft-prod.deb
+echo-information () {
+  echo -e "\e[34m ⓘ  $1\e[0m" >&2
+}
 
-# Clean up
-rm packages-microsoft-prod.deb
+echo-error () {
+  echo -e "\e[31m !  $1\e[0m" >&2
+}
 
-# Update packages
-sudo apt-get update
+echo-warning () {
+  echo -e "\e[33m ⚠  $1\e[0m" >&2
+}
 
-echo "➡️  Install .NET Runtime"
-sudo apt-get install dotnet-runtime-7.0 --yes --quiet
+echo-subsidiary () {
+  echo -e "\e[37m    $1\e[0m" >&2
+}
+
+echo-step () {
+  echo -e "\e[0m »  $1\e[0m" >&2
+}
+
+validate-is-root() {
+  if [[ $(/usr/bin/id -u) -ne 0 ]]
+  then
+    echo-error "Root is required"
+    exit 1
+  fi
+}
+
+validate-is-root
+
+echo-information "Installing ${PACKAGE}"
+
+echo-step "Add the Microsoft package repository"
+if ! (
+  set -e
+  declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+
+  # Download Microsoft signing key and repository
+  curl "https://packages.microsoft.com/config/ubuntu/${repo_version}/packages-microsoft-prod.deb" \
+    --location \
+    --output "${TEMP_FILE}" \
+    --show-error \
+    --silent
+
+  # Install Microsoft signing key and repository
+  dpkg -i "${TEMP_FILE}" > /dev/null
+  
+  # Clean up
+  rm "${TEMP_FILE}"
+  )
+then
+  echo-error "Failed to add the Microsoft package repository"
+  exit 1
+fi
+
+echo-step "Install .NET Runtime"
+if ! (
+  set -e
+  apt-get update --quiet
+  apt-get install ${PACKAGE} --yes --quiet
+  )
+then
+  echo-error "Failed to install .NET Runtime"
+  exit 1
+fi
+
+VERSION=$(${COMMAND} --version)
+echo-success "Installed ${PACKAGE}"
+echo-subsidiary "${VERSION}"
