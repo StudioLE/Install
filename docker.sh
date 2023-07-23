f97cada57c387742dd02c7a790237c66b02d8b73
Laurence Elsdon <git@elsdon.me>
2023-07-23 14:00:53 +0100

    feat(docker): Colorize the output and add error handling

--- docker.sh
+++ docker.sh
@@ -1,59 +1,132 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 # Install docker
 # Source: https://docs.docker.com/engine/install/ubuntu/
 
-echo "➡️  Uninstall old versions"
-set +e
+echo-success () {
+  echo -e "\e[32m ✔  $1\e[0m" >&2
+}
+
+echo-information () {
+  echo -e "\e[34m ⓘ  $1\e[0m" >&2
+}
+
+echo-error () {
+  echo -e "\e[31m !  $1\e[0m" >&2
+}
+
+echo-warning () {
+  echo -e "\e[33m ⚠  $1\e[0m" >&2
+}
+
+echo-subsidiary () {
+  echo -e "\e[37m    $1\e[0m" >&2
+}
+
+echo-step () {
+  echo -e "\e[0m »  $1\e[0m" >&2
+}
+
+validate-is-root() {
+  if [[ $(/usr/bin/id -u) -ne 0 ]]
+  then
+    echo-error "Root is required"
+    exit 1
+  fi
+}
+
+validate-not-installed() {
+  WHICH=$(which "${COMMAND}")  
+  if [[ "${WHICH}" != "" ]]
+  then
+    echo-warning "${COMMAND} is already installed:"
+    echo-subsidiary "Path: ${WHICH}"
+    exit 2
+  fi
+}
+
+validate-is-root
+validate-not-installed
+
+
+echo-information "Installing ${COMMAND}"
+echo-subsidiary "Source: ${SOURCE}"
+
+echo-step "Uninstall old versions"
 for PACKAGE in docker docker-engine docker.io containerd runc
 do
-  sudo apt-get remove "$PACKAGE" --quiet --yes
+  apt-get remove "${PACKAGE}" --quiet --yes
 done
-set -e
 
-echo "➡️  Install apt dependencies"
-sudo apt-get update
-sudo apt-get install --quiet --yes \
-  ca-certificates \
-  curl \
-  gnupg
+echo-step "Installing apt dependencies"
+if ! (
+  set -e
+  apt-get update --quiet
+  apt-get install --quiet --yes \
+    ca-certificates \
+    curl \
+    gnupg
+)
+then
+  echo-error "Failed to install apt dependencies"
+  exit 1
+fi
 
-echo "➡️  Add Docker official GPG key"
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl --fail --silent --show-error --location \
-  https://download.docker.com/linux/ubuntu/gpg \
-  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo-step "Setup the Docker repository"
+if ! (
+  set -e
+  mkdir -m 0755 -p /etc/apt/keyrings
+  curl --fail --silent --show-error --location \
+    https://download.docker.com/linux/ubuntu/gpg \
+    | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+)
+then
+  echo-error "Failed to setup the Docker repository"
+  exit 1
+fi
 
-echo "➡️  Set up the repository"
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo "➡️  Install Docker Engine"
-sudo apt-get update
-sudo apt-get install --quiet --yes \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-buildx-plugin \
-  docker-compose-plugin
-
+echo-step "Install Docker Engine"
+if ! (
+  set -e
+  apt-get update
+  apt-get install --quiet --yes \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
+)
+then
+  echo-error "Failed to setup the Docker repository"
+  exit 1
+fi
 
 # Post install steps
 # Source: https://docs.docker.com/engine/install/linux-postinstall/
 
-echo "➡️  Create the docker group"
-set +e
-sudo groupadd docker
-set -e
+echo-step "Create the docker group"
+if ! groupadd docker
+then
+  echo-warning "Failed to create the docker group"
+fi
 
-echo "➡️  Add user to group"
-sudo usermod -aG docker "${SUDO_USER}"
+echo-step "Add user to the docker group"
+if ! usermod -aG docker "${SUDO_USER}"
+then
+  echo-warning "Failed to add user the docker group"
+fi
 
-echo "➡️  Reload groups"
+echo-step "Reload groups"
 newgrp docker
+if ! newgrp docker
+then
+  echo-warning "Failed to reload group"
+fi
 
 # TODO: Change docker logging provider
 # Source: https://docs.docker.com/engine/install/linux-postinstall/#configure-default-logging-driver
