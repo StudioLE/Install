54174dbbbae2d98fd640e2a75d55e21cf06199dd
Laurence Elsdon <git@elsdon.me>
2023-07-23 12:01:44 +0100

    fix(kubernetes): Replace the failing Google Cloud public key with k8s key

--- kubernetes.sh
+++ kubernetes.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 # Install kubeadm, kubelet, and kubectl
-# Source:
+# Source: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
+# With amendment: https://github.com/kubernetes/website/issues/42144
 
 echo "➡️  Install apt dependencies"
 sudo apt-get update
@@ -11,11 +12,10 @@ sudo apt-get install --quiet --yes \
   curl \
   gnupg
 
-echo "➡️  Download the Google Cloud public signing key"
+echo "➡️  Download the public signing key"
 sudo curl --fail --silent --show-error --location \
   --output /etc/apt/keyrings/kubernetes-archive-keyring.gpg \
-  https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
+  https://dl.k8s.io/apt/doc/apt-key.gpg 
 
 echo "➡️  Add the Kubernetes apt repository"
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
