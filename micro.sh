6c2c0c5ee234d000c0841bfc4d5ceb08f4b6e380
Laurence Elsdon <git@elsdon.me>
2023-07-23 14:46:59 +0100

    feat(micro): Simplify and colorize the output

--- micro.sh
+++ micro.sh
@@ -1,38 +1,76 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 COMMAND="micro"
 SOURCE="https://github.com/zyedidia/micro/releases/download/v2.0.11/micro-2.0.11-amd64.deb"
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]
-then
-  echo "❗  Failed to install. Root is required." >&2
-  exit 1
-fi
+echo-success () {
+  echo -e "\e[32m ✔  $1\e[0m" >&2
+}
 
-set +e
-WHICH=$(which "${COMMAND}")
-set -e
+echo-information () {
+  echo -e "\e[34m ⓘ  $1\e[0m" >&2
+}
 
-if [[ "${WHICH}" != "" ]]
-then
-  echo "❗  Failed to install. ${COMMAND} is already installed:" >&2
-  echo "${WHICH}"
-  exit 1
-fi
+echo-error () {
+  echo -e "\e[31m !  $1\e[0m" >&2
+}
 
+echo-warning () {
+  echo -e "\e[33m ⚠  $1\e[0m" >&2
+}
+
+echo-subsidiary () {
+  echo -e "\e[37m    $1\e[0m" >&2
+}
+
+echo-step () {
+  echo -e "\e[0m »  $1\e[0m" >&2
+}
+
+validate-is-root() {
+  if [[ $(/usr/bin/id -u) -ne 0 ]]
+  then
+    echo-error "Root is required"
+    exit 1
+  fi
+}
+
+validate-not-installed() {
+  WHICH=$(which "${COMMAND}")  
+  if [[ "${WHICH}" != "" ]]
+  then
+    echo-warning "${COMMAND} is already installed:"
+    echo-subsidiary "Path: ${WHICH}"
+    exit 2
+  fi
+}
+
+validate-is-root
+validate-not-installed
+
+echo-information "Installing ${COMMAND}"
+echo-subsidiary "Source: ${SOURCE}"
+
+echo-step "Downloading package"
 TEMP_FILE=$(mktemp --dry-run)
+if ! curl "${SOURCE}" \
+       --location \
+       --output "${TEMP_FILE}" \
+       --show-error \
+       --silent
+then
+  echo-error "Failed to download package"
+  exit 1
+fi
 
-echo "Installing ${COMMAND}"
-echo "  Source: ${SOURCE}"
-#echo "  Destination: ${DESTINATION}"
+echo-step "Installing package"
+if ! dpkg -i "${TEMP_FILE}" > /dev/null
+then
+  echo-error "Failed to install package"
+  exit 1
+fi
 
-curl "${SOURCE}" \
-  --location \
-  --output "${TEMP_FILE}" \
-  --progress-bar
- 
-dpkg -i "${TEMP_FILE}"
-
-echo "Complete. Confirm with:"
-echo "  ${COMMAND} --version"
+VERSION=$(${COMMAND} --version)
+echo-success "Installed ${COMMAND}"
+echo-subsidiary "${VERSION}"
